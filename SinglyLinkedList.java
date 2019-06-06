56b250ea41087e32c36384d1123fd82b84379dd3
tkeating <tkeating@click2mail.com>
2019-06-06 09:59:44 -0400

    Working on Generics

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -1,5 +1,34 @@
+//This would be the perfect time to learn more about generics
+
 public class SinglyLinkedList {
     public static void main(String args[]) {
         
+    }
+
+    public class Node {
+
+        public <T> data;
+        public next;
+        public previous;
+
+        public static void main(string args[]) {
+
+        }
+
+        public Node getNext() {
+
+        }
+
+        public static setNext(<T> data) {
+
+        }
+
+        public Node getPrevious() {
+
+        }
+
+        public static setPrevious() {
+
+        }
     }
 }
