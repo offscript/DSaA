37b3519e8045d3b4429e364353b39f2a618f55f2
tkeating <tkeating@click2mail.com>
2019-06-07 10:18:32 -0400

    SinglyLinkedList w/Generics

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -1,34 +1,57 @@
 //This would be the perfect time to learn more about generics
 
-public class SinglyLinkedList {
-    public static void main(String args[]) {
-        
+public class SinglyLinkedList<T> {
+
+    public Node head;
+
+    public void prepend(T data) {
+        Node new_head = new Node(data);
+        new_head.setNext(head);
+        head = new_head;
     }
 
-    public class Node {
+    public class Node<T> {
 
-        public <T> data;
-        public next;
-        public previous;
+        public T data;
+        public Node next;
 
-        public static void main(string args[]) {
-
+        public Node(T data) {
+            this.setData(data);
         }
 
         public Node getNext() {
-
+            return this.next;
         }
 
-        public static setNext(<T> data) {
-
+        public void setNext(Node next) {
+            this.next = next;
         }
 
-        public Node getPrevious() {
-
+        public T getData() {
+            return data;
         }
 
-        public static setPrevious() {
-
+        public void setData(T data) {
+            this.data = data;
         }
+    }
+
+    public static void main(String[] args) {
+        SinglyLinkedList test = new SinglyLinkedList();
+        test.prepend("String");
+        System.out.println(test.head.getData());
+        test.prepend(12);
+        System.out.println(test.head.getData());
+        test.prepend(12.0);
+        System.out.println(test.head.getData());
+        test.prepend(null);
+        System.out.println(test.head.getData());
+        System.out.println(test.head.next.getData());
+        System.out.println(test.head.next.next.getData());
+        System.out.println(test.head.next.next.next.getData());
+
+        //Node test = new Node();
+        //test.setData("Hello World");
+        //System.out.println(test.getData());
     }
 }
